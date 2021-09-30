02dc04495e1c43005faf0e975c7c958479c5c410
ahmedalmaliki <ahmedalmalikiab@gmail.com>
2021-09-30 20:41:02 +0300

    Done

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,26 @@
 
 
 class Solution:
+    
+    def checker(self, s,l,r): 
+       while l >= 0 and r < len(s) and s[l] == s[r]:
+          l -= 1
+          r += 1
+     	 
+       return s[l+1:r]
+     
+    def longest_palindromic(self, s: str) -> str:
+        maximum = " "
+        for i in range(len(s)):
+             case_1 = self.checker(s, i, i)
+             if len(case_1) > len(maximum):
+              	maximum = case_1
+             case_2 = self.checker(s, i, i+1) 
+             if len(case_2) > len(maximum): 
+                  maximum = case_2 
+        return maximum
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+
+
+p = Solution()
+print(p.longest_palindromic("cbbd"))
